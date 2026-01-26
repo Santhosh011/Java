af38d8695bc4c9457993961d717da6759023fa0f
Santhosh011 <jsksanthoshkanna@gmail.com>
2026-01-26 21:27:34 +0530

    Spring Boot Commit for component scanning

--- Spring Boot Dev/01-SpringBoot-Overview/01-SpringBoot-Demo/src/main/java/com/mySpring/FirstApplication/springApp/SpringAppApplication.java
+++ Spring Boot Dev/01-SpringBoot-Overview/01-SpringBoot-Demo/src/main/java/com/mySpring/FirstApplication/springApp/SpringAppApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+//@SpringBootApplication(
+//		scanBasePackages = {"com.mySpring.FirstApplication.springApp",
+//				                  "com.mySpring.FirstApplication.utils"}
+//)
 @SpringBootApplication
 public class SpringAppApplication {
 
